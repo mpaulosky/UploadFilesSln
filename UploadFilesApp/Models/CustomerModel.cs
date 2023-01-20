41e97f2244f0f342da465808f19728bcc3cf5f4e
mpaulosky <matthew.paulosky@gmail.com>
2023-01-20 11:27:04 -0800

    Update with Video 3 of Uploading Files in Blazor

---
+++ UploadFilesApp/Models/CustomerModel.cs
@@ -0,0 +1,16 @@
+﻿namespace UploadFilesApp.Models;
+
+public class CustomerModel
+{
+
+	public int Id { get; set; }
+
+	public string UserName { get; set; } = "mpaulosky";
+
+	public string? FirstName { get; set; }
+
+	public string? LastName { get; set; }
+
+	public string? FileName { get; set; }
+
+}
