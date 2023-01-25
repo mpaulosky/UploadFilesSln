99130599dc00583c7158b1e3c064d22dcfaa45d6
mpaulosky <matthew.paulosky@gmail.com>
2023-01-25 08:30:16 -0800

    Session 5 Display Stored Images
    Add functionality to get customer records from SQL and Display on web page

--- UploadFilesLibrary/SqlDataAccess.cs
+++ UploadFilesLibrary/SqlDataAccess.cs
@@ -1,4 +1,5 @@
 ﻿using Dapper;
+
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 
@@ -6,26 +7,52 @@ namespace UploadFilesLibrary;
 
 public class SqlDataAccess : ISqlDataAccess
 {
-    private readonly IConfiguration _config;
 
-    public SqlDataAccess(IConfiguration config)
-    {
-        _config = config;
-    }
+	private readonly IConfiguration _config;
 
-    public async Task SaveData(
-        string storedProc,
-        string connectionName,
-        object parameters)
-    {
-        string connectionString = _config.GetConnectionString(connectionName)
-            ?? throw new Exception($"Missing connection string at {connectionName}");
+	public SqlDataAccess(IConfiguration config)
+	{
 
-        using var connection = new SqlConnection(connectionString);
+		_config = config;
 
-        await connection.ExecuteAsync(
-            storedProc,
-            parameters,
-            commandType: System.Data.CommandType.StoredProcedure);
-    }
+	}
+
+	public async Task<List<T>> LoadData<T>(
+			string storedProc,
+			string connectionName,
+			object? parameters)
+	{
+
+		string connectionString = _config.GetConnectionString(connectionName)
+		?? throw new Exception($"Missing connection string at {connectionName}");
+
+		using var connection = new SqlConnection(connectionString);
+
+		var rows = await connection.QueryAsync<T>(
+				storedProc,
+				parameters,
+				commandType: System.Data.CommandType.StoredProcedure);
+
+		return rows.ToList();
+
+	}
+
+	public async Task SaveData(
+			string storedProc,
+			string connectionName,
+			object parameters)
+	{
+
+		string connectionString = _config.GetConnectionString(connectionName)
+				?? throw new Exception($"Missing connection string at {connectionName}");
+
+		using var connection = new SqlConnection(connectionString);
+
+		await connection.ExecuteAsync(
+				storedProc,
+				parameters,
+				commandType: System.Data.CommandType.StoredProcedure);
+
+	}
+
 }
